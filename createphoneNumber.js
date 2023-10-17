e92675858525a76b53e7b2aefc23edd415e289c0
Nezerwa <eligrand2000@gmail.com>
2023-10-17 12:35:51 +0200

    Rempve console

--- createphoneNumber.js
+++ createphoneNumber.js
@@ -8,4 +8,4 @@ const createPhoneNumber = (numbers) => {
   )}) ${convertToString.slice(3, 6)}-${convertToString.slice(6, 10)}`;
   return newPhoneNumber;
 };
-console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]));
+
